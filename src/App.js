45f3b55c7a80d6b9d3c44dd2ffa77530276fa7d1
Rehan <ayoubrehan@gmail.com>
2020-02-09 00:05:13 -0500

    Created username and password TextBox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TextField, Button } from "@material-ui/core";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <TextField placeholder="Enter User Name" />
+        <br />
+        <TextField
+          id="standard-password-input"
+          label="Password"
+          type="password"
+        />
+        <br />
+        <Button variant="contained" color="primary">
+          submit
+        </Button>
+      </form>
     </div>
   );
 }
